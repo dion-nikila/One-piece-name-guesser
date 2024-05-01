c9cbcaba49f7c00f61cb3b2111aa8543b05691fc
dion-nikila <dionnikila330@gmail.com>
2024-05-01 22:36:50 +0530

    upload one piece game

---
+++ OnePiece/CharacterData.js
@@ -0,0 +1,9 @@
+const characters = [
+    { name: 'luffy', hint: 'He wants to become the Pirate King.' },
+    { name: 'zoro', hint: 'He is a swordsman with three swords style.' },
+    { name: 'chopper', hint: 'The strawhats\' friendly neighborhood doctor.' },
+    { name: 'ussopp', hint: 'A great warrior of the sea! (or so he says.)' },
+  ];
+  
+  export default characters;
+  
